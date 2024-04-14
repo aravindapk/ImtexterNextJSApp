e8be2f31dd66eab27daacc79172facf9e5fe07dc
aravindapk <48381916+aravindapk@users.noreply.github.com>
2024-04-14 22:12:57 +0530

    Delete imtexter/app/lib/Utils/CursorCurator.ts
    deleted duplicate file

--- imtexter/app/lib/Utils/CursorCurator.ts
+++
@@ -1,7 +0,0 @@
-
-//used for pointing the cursor to the referred position on edit
- export const applyPrevCusrorPointer = (position: HTMLInputElement["selectionStart"], inputRef : React.MutableRefObject<any>) => {
-    requestAnimationFrame(() => {
-        inputRef.current.selectionStart = inputRef.current.selectionEnd = position;
-    });
-   }
